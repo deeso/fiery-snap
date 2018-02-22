c64363cea1dd524e3c0e0046eb704b4e7c47001f
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-02-22 12:32:26 -0600

    make it easier to start sink types separately

---
+++ docker/sinks/setup_sinks_email.sh
@@ -0,0 +1,7 @@
+cd pastebin-scraper-email-results
+sh run-file.sh
+cd ..
+
+cd twitter-scraper-email-results
+sh run-file.sh
+cd ..
---
+++ docker/sinks/setup_sinks_mongo.sh
@@ -0,0 +1,15 @@
+cd mongo-raw-sink
+sh run-file.sh
+cd ..
+
+cd mongo-processed-sink
+sh run-file.sh
+cd ..
+
+cd mongo-raw-sink-pastebin
+sh run-file.sh
+cd ..
+
+cd mongo-processed-sink-pastebin
+sh run-file.sh
+cd ..
